a6528ece052fe705c38a72a90a8899fb76632d30
futomakiyoin <72528988+futomaki-bit@users.noreply.github.com>
2022-04-26 09:07:48 -0400

    Added algorithm for grocery data

--- scrapper.js
+++ scrapper.js
@@ -1,8 +1,11 @@
 // Guide: https://zetcode.com/javascript/cheerio/
 
-const cheerio = require('cheerio');
-const request = require('request');
+const cheerio = require('cheerio')
+const request = require('request')
+const express = require('express')
 const fs = require('fs')
+
+const app = express()
 
 request({
   method: 'GET',
@@ -16,16 +19,25 @@ request({
   let filter = $('section[class="post_body"] > div > div[class="content"] ');
 
   write(filter.text());
+  organize();
 });
 
 // https://nodejs.dev/learn/writing-files-with-nodejs
 
 function write(content) {
-  fs.writeFile('./test.txt', content, err => {
+  fs.writeFile('./groceries.txt', content, err => {
     if (err) {
       console.error(err)
       return
     }
     //file written successfully
   })
+}
+
+function organize() {
+  let {PythonShell} = require('python-shell')
+  PythonShell.run('./organize.py', null, function (err) {
+    if (err) throw err;
+    console.log('finished');
+  });
 }
